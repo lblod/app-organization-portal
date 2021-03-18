3bffa7778cc290d0ad7cf9146e9dd9215f30154a
Nordine Bittich <nordine@bittich.be>
2021-03-18 09:26:43 +0100

    Metis & frontend

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -11,6 +11,43 @@ defmodule Dispatcher do
   define_layers [ :api, :frontend, :not_found ]
 
   ###############################################################
+  # sparql endpoint
+  ###############################################################
+  match "/sparql/*path", %{ accept: [:any], layer: :api} do
+    forward conn, path, "http://db:8890/sparql/"
+  end
+
+  ###############################################################
+  # ember metis
+  ###############################################################
+  get "/uri-info/*path", %{ accept: [:json], layer: :api} do
+    forward conn, path, "http://uri-info/"
+  end
+
+  get "/resource-labels/*path", %{ accept: [:json], layer: :api} do
+    forward conn, path, "http://resource-labels/"
+  end  
+
+  ###############################################################
+  # frontend layer
+  ###############################################################
+  match "/assets/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/@appuniversum/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  end
+
+  match "/*path", %{ accept: [:html], layer: :api } do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/*_path", %{ layer: :frontend } do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  ###############################################################
   # errors
   ###############################################################
   match "/*_path", %{ accept: [:any], layer: :not_found} do
