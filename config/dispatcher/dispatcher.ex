d082453ac53541b4adc74c26d0eec771441b5ffe
anitacaron <anitarcaron@gmail.com>
2021-06-17 08:59:05 +0100

    Include missing path and prefix

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -125,6 +125,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resource/organization-status-codes/"
   end   
 
+  match "/locations/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://resource/locations/"
+  end
+
   ###############################################################
   # sparql endpoint
   ###############################################################
