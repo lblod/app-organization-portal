00432e94462910256ee783a6150b9ab5483cac12
felix <ruiz.felixruiz@gmail.com>
2021-11-16 16:43:43 +0100

    Delta-producer have multiple background-jobs with different tasks
    Hence the introduction of the jobs-controller.
    Note: in a more advanced configuration, you can easily extend the
    tasks related to one job.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -14,5 +14,23 @@ export default [
         gracePeriod: 1000,
         ignoreFromSelf: true
       }
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      }
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://jobs-controller/delta'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
+  },
     }
   ]
