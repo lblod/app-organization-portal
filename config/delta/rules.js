33f5090de0f7c42fd8f323e6469a5b6959e8ae50
felix <ruiz.felixruiz@gmail.com>
2021-11-16 16:58:19 +0100

    Introduction of delta-producer-publication-graph-maintainer
    This service is the core of the logic which determines what should be
    published.
    The configuration is provided here, and can be elaborated refined in
    the config.
    It also excutes the healing logic.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -34,6 +34,23 @@ export default [
   },
   {
     match: {
+    },
+    callback: {
+      url: 'http://delta-producer-pub-graph-maintainer-administrative-units/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+                        ]
+    }
+  },
+  {
+    match: {
       predicate: {
         type: 'uri',
         value: 'http://www.w3.org/ns/adms#status'
