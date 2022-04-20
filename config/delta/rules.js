5537b27db1e47f29bb8d245752b6e3ff2f059a06
Nordine Bittich <nordine@bittich.be>
2022-04-20 11:14:07 +0200

    fix caching issues

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -18,6 +18,23 @@ export default [
   },
   {
     match: {
+      graph: {
+        type: "uri",
+        value: "http://mu.semte.ch/graphs/organisatieportaal"
+      }
+    },
+    callback: {
+      url: 'http://resource/.mu/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
       predicate: {
         type: 'uri',
         value: 'http://www.w3.org/ns/adms#status'
