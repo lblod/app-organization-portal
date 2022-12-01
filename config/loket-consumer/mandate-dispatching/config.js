e37df1387675ba538fa7a72020aac6515241175e
Boris De Vloed <code@devloed.com>
2022-12-01 13:52:35 +0000

    use smarter default ingest graph

--- config/loket-consumer/mandate-dispatching/config.js
+++ config/loket-consumer/mandate-dispatching/config.js
@@ -5,7 +5,7 @@ const DIRECT_DATABASE_ENDPOINT = process.env.DIRECT_DATABASE_ENDPOINT || 'http:/
 const MAX_DB_RETRY_ATTEMPTS = parseInt(process.env.MAX_DB_RETRY_ATTEMPTS || 5);
 const SLEEP_BETWEEN_BATCHES = parseInt(process.env.SLEEP_BETWEEN_BATCHES || 1000);
 const SLEEP_TIME_AFTER_FAILED_DB_OPERATION = parseInt(process.env.SLEEP_TIME_AFTER_FAILED_DB_OPERATION || 60000);
-const INGEST_GRAPH = process.env.INGEST_GRAPH || `http://mu.semte.ch/graphs/public`;
+const INGEST_GRAPH = process.env.INGEST_GRAPH || `http://mu.semte.ch/graphs/organisatieportaal`;
 
 
 if(!process.env.FILE_SYNC_GRAPH)
