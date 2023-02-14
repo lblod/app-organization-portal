335501ede6a650d2f322fd2e08319425471170f9
Elena Poelman <elena.poelman@redpencil.io>
2023-02-14 12:34:57 +0100

    use custom dispatch method to send organization details to ldes endpoint

--- config/ldes-delta-pusher/dispatch.ts
+++ config/ldes-delta-pusher/dispatch.ts
@@ -1,13 +1,29 @@
 import { moveTriples } from "../support";
 import { Changeset } from "../types";
+import { query, sparqlEscapeUri } from 'mu';
 
 export default async function dispatch(changesets: Changeset[]) {
 	for (const changeset of changesets) {
-		await moveTriples([
-			{
-				inserts: changeset.inserts,
-				deletes: changeset.deletes,
-			},
-		]);
+		const subjects = new Set(changeset.inserts.map((insert) => insert.subject.value));
+		for (const subject of subjects) {
+			const { results: { bindings } } = await query(`
+				PREFIX org: <http://www.w3.org/ns/org#>
+				CONSTRUCT {
+					${sparqlEscapeUri(subject)} a org:Organization;
+																			?p ?o.
+				}
+        WHERE { 
+          ${sparqlEscapeUri(subject)} a org:Organization;
+                                      ?p ?o.
+        }
+			`);
+      if(bindings.length){
+        await moveTriples([
+          {
+            inserts: bindings.map(({ s, p, o}) => { return { subject: s, predicate: p, object: o} }),
+          }
+        ])
+      }
+		}
 	}
 }
