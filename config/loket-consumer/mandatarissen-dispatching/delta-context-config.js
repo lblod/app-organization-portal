f26a61fb39b689fe65268b06d14b66e2d6a0f94c
Claire Lovisa <lovisa.claire@gmail.com>
2023-05-15 14:39:43 +0200

    Fix context config

--- config/loket-consumer/mandatarissen-dispatching/delta-context-config.js
+++ config/loket-consumer/mandatarissen-dispatching/delta-context-config.js
@@ -1,7 +1,7 @@
 const PREFIXES = `
 PREFIX org: <http://www.w3.org/ns/org#>
 PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
-PREFIX lblodlg: <https://data.lblod.info/vocabularies/leidinggevenden/>
+PREFIX lblodlg: <http://data.lblod.info/vocabularies/leidinggevenden/>
 PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>`
 
 const contextConfig = {
@@ -10,55 +10,7 @@ const contextConfig = {
     exhausitive: false, // true or false: find all types for a subject, even if one is already present in delta
   },
   contextQueries: [
-    // FIRST CASE : Leidnggevenden
-    // They can be triggered when being inserted or when their link to a bestuursfunctie is added
-    {
-      trigger: { // subjectType or predicateValue
-        subjectType: "lblodlg:Functionaris"
-      },
-      queryTemplate: (subject) => `
-        ${PREFIXES}
-        CONSTRUCT {
-          ?functionaris org:holds ?bestuursfunctie .
-          ?orgaanInTime lblodlg:heeftBestuursfunctie ?bestuursfunctie ;
-            mandaat:isTijdspecialisatieVan ?orgaan .
-          ?orgaan besluit:bestuurt ?eenheid .
-          ?eenheid besluit:classificatie ?classification .
-        } WHERE {
-          GRAPH <http://mu.semte.ch/graphs/landing-zone/leidinggevenden> {
-            VALUES ?functionaris { ${subject} }
-            ?functionaris org:holds ?bestuursfunctie .
-            ?orgaanInTime lblodlg:heeftBestuursfunctie ?bestuursfunctie ;
-              mandaat:isTijdspecialisatieVan ?orgaan .
-            ?orgaan besluit:bestuurt ?eenheid .
-            ?eenheid besluit:classificatie ?classification .
-          }
-        }`
-    },
-    {
-      trigger: {
-        predicateValue: "org:holds"
-      },
-      queryTemplate: (subject) => `
-        ${PREFIXES}
-        CONSTRUCT {
-          ?functionaris org:holds ?bestuursfunctie .
-          ?orgaanInTime lblodlg:heeftBestuursfunctie ?bestuursfunctie ;
-            mandaat:isTijdspecialisatieVan ?orgaan .
-          ?orgaan besluit:bestuurt ?eenheid .
-          ?eenheid besluit:classificatie ?classification .
-        } WHERE {
-          GRAPH <http://mu.semte.ch/graphs/landing-zone/leidinggevenden> {
-            VALUES ?functionaris { ${subject} }
-            ?functionaris org:holds ?bestuursfunctie .
-            ?orgaanInTime lblodlg:heeftBestuursfunctie ?bestuursfunctie ;
-              mandaat:isTijdspecialisatieVan ?orgaan .
-            ?orgaan besluit:bestuurt ?eenheid .
-            ?eenheid besluit:classificatie ?classification .
-          }
-        }`
-    },
-    // SECOND CASE : Mandatarissen
+    // Case Mandatarissen
     // They can be triggered when being inserted or when their link to a mandate is added
     {
       trigger: { // subjectType or predicateValue
@@ -105,7 +57,7 @@ const contextConfig = {
             ?eenheid besluit:classificatie ?classification .
           }
         }`
-    },
+    }
   ]
 }
 
