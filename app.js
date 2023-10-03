0cb98aed1ed64836f3ecdea46fea21ab339aa033
Claire Lovisa <lovisa.claire@gmail.com>
2023-10-03 15:46:57 +0200

    Use endpoint instead of deltas for live updates

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 import { app, errorHandler } from 'mu';
-import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
 import { CronJob } from 'cron';
-import { Delta } from "./lib/delta";
 import {
-  isKbo,
   getIdentifiers,
   constructOvoStructure,
   updateOvoNumberAndUri,
@@ -12,59 +9,38 @@ import {
 } from "./lib/queries";
 import { CRON_PATTERN } from './config';
 
-app.use(bodyParser.json({
-  type: function (req) {
-    return /^application\/json/.test(req.get('content-type'));
-  }
-}));
-
 // TODO update readme with env variables etc at the end
 
-// TODO current issue: when the KBO get updated, it doesn't refresh in the frontend.
-// Alternative: endpoint to call in the frontend directly, that way we can trigger a refresh on the OVO ?
-app.post('/delta', async function (req, res, next) {
+app.post('/sync-from-kbo/:kboStructuredIdUuid', async function(req, res) {
   try {
-    const entries = new Delta(req.body).getInsertsFor('https://data.vlaanderen.be/ns/generiek#lokaleIdentificator');
+    const kboStructuredIdUuid = req.params.kboStructuredIdUuid;
+    const identifiers = await getIdentifiers(kboStructuredIdUuid);
 
-    if (!entries.length) {
-      console.log('Delta dit not contain potential KBOs, awaiting the next batch!');
-      return res.status(204).send();
+    const response = await fetch(`https://api.wegwijs.vlaanderen.be/v1/search/organisations?q=kboNumber:${identifiers.kbo}&fields=ovoNumber,kboNumber)`);
+    const data = await response.json();
+
+    let wegwijsOvo = null;
+    if (data.length) {
+      // We got a match on the KBO, getting the associated OVO back
+      const wegwijsInfo = data[0]; // Wegwijs should only have only one entry per KBO
+      if (wegwijsInfo.ovoNumber)
+        wegwijsOvo = wegwijsInfo.ovoNumber;
     }
 
-    for (let entry of entries) {
-      const isEntryKbo = await isKbo(entry)
+    if (wegwijsOvo && (wegwijsOvo != identifiers.ovo)) {
+      let ovoStructuredIdUri = identifiers.ovoStructuredId;
 
-      if (isEntryKbo) {
-        const identifiers = await getIdentifiers(entry);
-
-        const response = await fetch(`https://api.wegwijs.vlaanderen.be/v1/search/organisations?q=kboNumber:${identifiers.kbo}&fields=ovoNumber,kboNumber)`);
-        const data = await response.json();
-
-        let wegwijsOvo = null;
-        if (data.length) {
-          // We got a match on the KBO, getting the associated OVO back
-          const wegwijsInfo = data[0]; // Wegwijs should only have only one entry per KBO
-          if (wegwijsInfo.ovoNumber)
-            wegwijsOvo = wegwijsInfo.ovoNumber;
-        }
-
-        if (wegwijsOvo && (wegwijsOvo != identifiers.ovo)) {
-          let ovoStructuredIdUri = identifiers.ovoStructuredId;
-
-          if (!ovoStructuredIdUri) {
-            ovoStructuredIdUri = await constructOvoStructure(entry);
-          }
-
-          await updateOvoNumberAndUri(ovoStructuredIdUri, wegwijsOvo);
-        }
+      if (!ovoStructuredIdUri) {
+        ovoStructuredIdUri = await constructOvoStructure(identifiers.kboStructuredId);
       }
+
+      await updateOvoNumberAndUri(ovoStructuredIdUri, wegwijsOvo);
     }
 
-    return res.status(200).send().end();
+    return res.status(200).send(); // since we await, it should be 200
   } catch (e) {
-    console.log(`Something unexpected went wrong while handling delta task!`);
-    console.error(e);
-    return next(e);
+    console.log("Something went wrong while calling /sync-from-kbo", e);
+    return res.status(500).send();
   }
 });
 
