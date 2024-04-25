94baeb539f3d58f42e28d9968192d17fb1aecb2a
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-04-25 15:56:56 +0200

    chore: Bump delta-producer-publication-graph-maintainer to 1.0.9-rc2 + enable other producers

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,5 +1,40 @@
 export default [
-  /*{
+  {
+    match: {
+      graph: {
+        type: "uri",
+        value: "http://mu.semte.ch/graphs/administrative-unit"
+      }
+    },
+    callback: {
+      url: 'http://search/update',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 10000,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      graph: {
+        type: "uri",
+        value: "http://mu.semte.ch/graphs/worship-service"
+      }
+    },
+    callback: {
+      url: 'http://search/update',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 10000,
+      ignoreFromSelf: true
+    }
+  },
+
+  {
     match: {
       graph: {
         type: "uri",
@@ -34,7 +69,7 @@ export default [
       ignoreFromSelf: true
     }
   },
-*/
+
   {
     match: {
       predicate: {
@@ -70,7 +105,58 @@ export default [
       ]
     }
   },
-  
+  {
+    match: {
+    },
+    callback: {
+      url: 'http://delta-producer-pub-graph-maintainer-organizations-public-info/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+        "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+        "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+      ]
+    }
+  },
+  {
+    match: {
+    },
+    callback: {
+      url: 'http://delta-producer-pub-graph-maintainer-public/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+        "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+        "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+      ]
+    }
+  },
+  {
+    match: {
+      // anything
+    },
+    callback: {
+      url: 'http://delta-producer-pub-graph-maintainer-worship-posts/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+        "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+        "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+      ]
+    }
+  },
   {
     match: {
       predicate: {
@@ -94,60 +180,60 @@ export default [
     }
   },
   // organizations-sync-with-sharepoint/delta : jobs come from the jobs graph, data from the two main graphs
-  // {
-  //   match: {
-  //     graph: {
-  //       type: 'uri',
-  //       value: 'http://mu.semte.ch/graphs/jobs'
-  //     }
-  //   },
-  //   callback: {
-  //     url: 'http://organizations-sync-with-sharepoint/delta',
-  //     method: 'POST'
-  //   },
-  //   options: {
-  //     resourceFormat: 'v0.0.1',
-  //     gracePeriod: 1000,
-  //     ignoreFromSelf: true,
-  //     optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
-  //   }
-  // },
-  // {
-  //   match: {
-  //     graph: {
-  //       type: 'uri',
-  //       value: 'http://mu.semte.ch/graphs/shared'
-  //     }
-  //   },
-  //   callback: {
-  //     url: 'http://organizations-sync-with-sharepoint/delta',
-  //     method: 'POST'
-  //   },
-  //   options: {
-  //     resourceFormat: 'v0.0.1',
-  //     gracePeriod: 1000,
-  //     ignoreFromSelf: true,
-  //     optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
-  //   }
-  // },
-  // {
-  //   match: {
-  //     graph: {
-  //       type: 'uri',
-  //       value: 'http://mu.semte.ch/graphs/administrative-unit'
-  //     }
-  //   },
-  //   callback: {
-  //     url: 'http://organizations-sync-with-sharepoint/delta',
-  //     method: 'POST'
-  //   },
-  //   options: {
-  //     resourceFormat: 'v0.0.1',
-  //     gracePeriod: 1000,
-  //     ignoreFromSelf: true,
-  //     optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
-  //   }
-  // }
+  {
+    match: {
+      graph: {
+        type: 'uri',
+        value: 'http://mu.semte.ch/graphs/jobs'
+      }
+    },
+    callback: {
+      url: 'http://organizations-sync-with-sharepoint/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
+  },
+  {
+    match: {
+      graph: {
+        type: 'uri',
+        value: 'http://mu.semte.ch/graphs/shared'
+      }
+    },
+    callback: {
+      url: 'http://organizations-sync-with-sharepoint/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
+  },
+  {
+    match: {
+      graph: {
+        type: 'uri',
+        value: 'http://mu.semte.ch/graphs/administrative-unit'
+      }
+    },
+    callback: {
+      url: 'http://organizations-sync-with-sharepoint/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
+  }
 /*
   Commenting ldes rules until ready to go to qa/prod. In the meantime, we override the configuration on dev.
   {
